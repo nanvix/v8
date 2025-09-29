227ade3ed7f191c0640b9d96678f632c2e8d4beb
Pedro Henrique Penna <ppenna@microsoft.com>
2025-09-29 14:37:52 -0700

    Fix memory allocator

--- src/base/platform/memory.h
+++ src/base/platform/memory.h
@@ -21,20 +21,13 @@
 #include <malloc/malloc.h>
 #elif V8_OS_OPENBSD
 #include <sys/malloc.h>
-#elif V8_OS_ZOS
+#elif V8_OS_ZOS || V8_OS_NANVIX
 #include <stdlib.h>
 #else
 #include <malloc.h>
 #endif
 
-#if V8_OS_NANVIX
-extern "C" {
-void* __aligned_malloc(size_t size, size_t alignment);
-void __aligned_free(void* ptr);
-}  // extern "C"
-#endif  // V8_OS_NANVIX
-
-#if (V8_OS_POSIX && !V8_OS_AIX && !V8_OS_SOLARIS && !V8_OS_ZOS && !V8_OS_OPENBSD) || V8_OS_WIN
+#if (V8_OS_POSIX && !V8_OS_AIX && !V8_OS_SOLARIS && !V8_OS_ZOS && !V8_OS_OPENBSD && !V8_OS_NANVIX) || V8_OS_WIN
 #define V8_HAS_MALLOC_USABLE_SIZE 1
 #endif
 
@@ -97,8 +90,10 @@ inline void* AlignedAlloc(size_t size, size_t alignment) {
   // posix_memalign is not exposed in some Android versions, so we fall back to
   // memalign. See http://code.google.com/p/android/issues/detail?id=35391.
   return memalign(alignment, size);
-#elif V8_OS_ZOS || V8_OS_NANVIX
+#elif V8_OS_ZOS
   return __aligned_malloc(size, alignment);
+#elif V8_OS_NANVIX
+  return aligned_alloc(alignment, size);
 #else   // POSIX
   void* ptr;
   if (posix_memalign(&ptr, alignment, size)) ptr = nullptr;
@@ -109,7 +104,7 @@ inline void* AlignedAlloc(size_t size, size_t alignment) {
 inline void AlignedFree(void* ptr) {
 #if V8_OS_WIN
   _aligned_free(ptr);
-#elif V8_OS_ZOS || V8_OS_NANVIX
+#elif V8_OS_ZOS
   __aligned_free(ptr);
 #else
   // Using regular Free() is not correct in general. For most platforms,
